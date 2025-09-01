2e47f0bae8826cb27bc00c4ec48b5fd67a8f7494
glenn <glennalverez@gmail.com>
2025-09-01 15:31:41 -0500

    ejercicios

--- src/main/java/com/example/Ejercicio10.java
+++ src/main/java/com/example/Ejercicio10.java
@@ -5,24 +5,25 @@ public class Ejercicio10 {
     public static void ejercicio10() {
         System.out.println("Ejercicio 10");
 
-        Scanner scanner = new Scanner(System.in);
-
         final double PRECIO_PRODUCTO = 15.99;
         final int IVA_PORCENTAJE = 16;
 
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese la cantidad de productos a comprar: ");
-        int cantidadDeProductos = input.nextInt();
+        int cantidadDeProductos = scanner.nextInt();
 
-        Scanner input2 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
         System.out.print("nombre del cliente:");
-        String nombreCliente = input.next();
+        String nombreCliente = scanner.next();
 
         double subtotal = PRECIO_PRODUCTO * cantidadDeProductos;
         double iva = subtotal * IVA_PORCENTAJE / 100;
         double total = subtotal + iva;
 
         System.out.println("Factura de compra" + "Cliente: " + nombreCliente + "\n" + "Cantidad de productos: " + cantidadDeProductos + "\n" + "Precio producto: $" + PRECIO_PRODUCTO + "\n" + "Subtotal: $" + subtotal + "\n" + "IVA (16%): $" + iva + "\n" + "Total a pagar: $" + total);
+
+        scanner.close();
+        scanner2.close();
       
     }
 
