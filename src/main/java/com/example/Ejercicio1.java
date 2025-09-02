da266f77b6b17ee35f338a6f0973e4349fef7046
KingAskhar <dramirez12345@cesde.net>
2025-09-02 18:14:10 -0500

    Ejercicio 1

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -3,6 +3,17 @@ package com.example;
 public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
-        // tu código aquí
+        int edad = 2;
+        
+        System.out.println("Su edad es " + edad);
+        double altura = 1.75f;
+
+        System.out.println("Su altura es " + altura);
+        String nombre = "David";
+        System.out.println("Su nombre es " + nombre);
+
+        boolean Estudiante = true;
+        System.out.println("Su estado es " + Estudiante);
     }
+
 }
