0f659730b70cae8e4a84da27afc060d315f7f9a1
JD0P <1040743909@amigomed2.funlam.edu.co>
2025-09-02 15:19:45 -0500

    Solución ejercicios 1 a 5

--- src/main/java/com/example/Ejercicio2.java
+++ src/main/java/com/example/Ejercicio2.java
@@ -2,7 +2,25 @@ package com.example;
 
 public class Ejercicio2 {
     public static void ejercicio2() {
-        System.out.println("Ejercicio 2");
-        // tu código aquí
+
+        //Variables
+        byte byteValue = 100;
+        short shortValue = 1000;
+        int intValue = 50000;
+        long longValue = 1000000L; 
+        float floatValue = 3.14f;
+        double doubleValue = 2.71828;
+        char charValue = 'A';
+        boolean booleanValue = false;
+
+        //Imprimir variables
+        System.out.println(byteValue);
+        System.out.println(shortValue);
+        System.out.println(intValue);
+        System.out.println(longValue);
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+        System.out.println(charValue);
+          System.out.println(booleanValue);
     }
 }
--- src/main/java/com/example/Ejercicio5.java
+++ src/main/java/com/example/Ejercicio5.java
@@ -2,7 +2,17 @@ package com.example;
 
 public class Ejercicio5 {
     public static void ejercicio5() {
-        System.out.println("Ejercicio 5");
-        // tu código aquí
+
+        //Imprimir en consola "Hola"
+        String mensaje1 = "Hola";
+        System.out.print(mensaje1);
+        
+        //Imprimir en consola "Mundo"
+        String mensaje2 = "Mundo";
+        System.out.println(mensaje2);
+        
+        //Imprimir en consola "Mi edad es %d años""
+        int edad = 32;
+        System.out.println("Mi edad es " + edad + " años.");
     }
 }
