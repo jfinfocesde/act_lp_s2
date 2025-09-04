3985dbe4659735d2c09884ee48599ebdf7b4fc55
JD0P <jospinada@cesde.net>
2025-09-04 10:00:38 -0500

    Solución ejercicios 6 y 7

--- src/main/java/com/example/Ejercicio6.java
+++ src/main/java/com/example/Ejercicio6.java
@@ -1,8 +1,36 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
     public static void ejercicio6() {
-        System.out.println("Ejercicio 6");
-        // tu código aquí
+
+        //Variables
+        String nombre;
+        int edad;
+        double altura; 
+
+        //Entrada de datos
+        Scanner scanner = new Scanner(System.in);
+
+        //Solicitar nombre
+        System.out.println("Escriba su nombre");
+        nombre = scanner.nextLine();
+
+        //Solicitar edad
+        System.out.println("Ingrese su edad");
+        edad = scanner.nextInt();
+
+        //Solicitar altura 
+        System.out.println("Ingrese su altura");
+        altura = scanner.nextDouble();
+
+        //Imprimir datos
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+
+        //Cerrar Scanner
+        scanner.close();
     }
 }
--- src/main/java/com/example/Ejercicio7.java
+++ src/main/java/com/example/Ejercicio7.java
@@ -2,7 +2,17 @@ package com.example;
 
 public class Ejercicio7 {
     public static void ejercicio7() {
-        System.out.println("Ejercicio 7");
-        // tu código aquí
+
+        //Variables
+        String nombre = "Juan";
+        String apellido = "Perez"; 
+        String nombreCompleto = nombre + " " + apellido; 
+        String saludo = "Hola ".concat(nombreCompleto);
+        int edad = 20;
+        String mensajeEdad = "Tengo " + edad + " años."; 
+
+        //Imprimir mensajes
+        System.out.println(saludo);
+        System.out.println(mensajeEdad);
     }
 }
