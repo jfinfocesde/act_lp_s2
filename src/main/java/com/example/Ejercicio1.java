723b8c5b365efb0c0b4926d7464e71b425fed839
KingAskhar <dramirez12345@cesde.net>
2025-09-05 22:07:03 -0500

    actualizacion ejercicios

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -3,17 +3,18 @@ package com.example;
 public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
-        int edad = 2;
         
+        int edad = 25;
         System.out.println("Su edad es " + edad);
-        double altura = 1.75f;
 
+        double altura = 1.75;
         System.out.println("Su altura es " + altura);
+
         String nombre = "David";
         System.out.println("Su nombre es " + nombre);
 
         boolean Estudiante = true;
-        System.out.println("Su estado es " + Estudiante);
+        System.out.println("Es estudiante  " + Estudiante);
     }
 
 }
--- src/main/java/com/example/Ejercicio4.java
+++ src/main/java/com/example/Ejercicio4.java
@@ -4,5 +4,6 @@ public class Ejercicio4 {
     public static void ejercicio4() {
         System.out.println("Ejercicio 4");
         // tu código aquí
+        
     }
 }
