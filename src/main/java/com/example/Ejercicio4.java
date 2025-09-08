2fe39428754f838e342fef48cfa0d6100a3ae1f2
KingAskhar <dramirez12345@cesde.net>
2025-09-08 10:28:39 -0500

    Update Ejercicio4.java

--- src/main/java/com/example/Ejercicio4.java
+++ src/main/java/com/example/Ejercicio4.java
@@ -3,6 +3,16 @@ package com.example;
 public class Ejercicio4 {
     public static void ejercicio4() {
         System.out.println("Ejercicio 4");
-        // tu código aquí
+        final double PI = 3.14159;
+        System.out.println("PI vale " + PI);
+
+        final int DIAS_SEMANA = 7;
+        System.out.println("Los dias de la semana son " + DIAS_SEMANA);
+
+        final String NOMBRE_UNIVERSIDAD = "Cesde";
+        System.out.println("El nombre de la universidad " + NOMBRE_UNIVERSIDAD);
+
+        final double area = PI * (5*5);
+        System.out.println("El area de un circulo de radiio 5 es " + area);
     }
 }
