fcfa481088f212072cfb39c54e42b11a1f99b462
Yor471 <yorladismotatogrisales@gmail.com>
2025-09-11 16:25:08 -0500

    Update Ejercicio2.java

--- src/main/java/com/example/Ejercicio2.java
+++ src/main/java/com/example/Ejercicio2.java
@@ -3,6 +3,29 @@ package com.example;
 public class Ejercicio2 {
     public static void ejercicio2() {
         System.out.println("Ejercicio 2");
-        // tu código aquí
+        byte numero = 100;
+    System.out.println("el numero es " + numero);
+
+    short numero2 = 10000;
+    System.out.println("el numero es " + numero2);
+
+    int numero3 = 50000;
+    System.out.println("el numero es " + numero3);
+    
+    Long numero4 = 1000000000L;
+System.out.println("el numero es " + numero4);
+
+    float numero5 = 3.14f;
+    System.out.println("el numero es " + numero5);
+
+    double numero6 = 2.71828;
+    System.out.println("el numero es " + numero6);
+
+    char letra = 'A';
+    System.out.println("la letra es " + letra);
+
+    boolean esVerdadero = true;
+    System.out.println("el valor es " + esVerdadero);
+// tu código aquí
     }
 }
