f6c6297e0650e4c8485881a88133a39526499368
talzateg-netizen <talzateg@gmail.com>
2025-09-13 15:04:26 -0500

    Update Ejercicio1.java

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -3,6 +3,22 @@ package com.example;
 public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
-        // tu código aquí
+
+ int edad = 25;                    // variable de tipo int
+        double altura = 1.75;              // variable de tipo double
+        String nombre = "Tania Alzate";    // variable de tipo String
+        boolean esEstudiante = true;      // variable de tipo boolean
+        
+        // Impresión de los valores de las variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Es estudiante: " + esEstudiante);
+    }
+
+    public static void main(String[] args) {
+        ejercicio1();  // Llamada al método ejercicio1
+
+        
     }
 }
