92e955e79a64d9a4a9f026b37d995013e977281f
joseleonardosg19-ship-it <joseleonardosg19@gmail.com>
2025-09-13 13:53:44 -0500

    Update Ejercicio1.java

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -3,6 +3,15 @@ package com.example;
 public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
-        // tu código aquí
+        // tint edad = 25;
+        double altura = 1.75;
+        String nombre = "jose leonardo saez";
+        boolean esEstudiante = true;
+
+    System.out.println(edad);
+    System.out.println(altura);
+    System.out.println(esEstudiante);
+    System.out.println(nombre);    
+         
     }
 }
