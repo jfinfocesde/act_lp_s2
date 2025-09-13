4b159e3c4a2aefa8b5b638fc0ef140a2acc08141
joseleonardosg19-ship-it <joseleonardosg19@gmail.com>
2025-09-13 14:04:44 -0500

    Update Ejercicio5.java

--- src/main/java/com/example/Ejercicio5.java
+++ src/main/java/com/example/Ejercicio5.java
@@ -4,5 +4,13 @@ public class Ejercicio5 {
     public static void ejercicio5() {
         System.out.println("Ejercicio 5");
         // tu código aquí
-    }
-}
+    
+        int edad = 28;
+     
+        System.out.print("Hola" );
+       System.out.println("\nMundo");
+       System.out.printf("Mi edad es %d años", edad);
+       System.out.println("-----------------------");
+
+    {
+
