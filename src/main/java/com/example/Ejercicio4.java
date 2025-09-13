2faa01ecb09298d085a152be79be353aaa52e34c
joseleonardosg19-ship-it <joseleonardosg19@gmail.com>
2025-09-13 14:04:15 -0500

    Update Ejercicio4.java

--- src/main/java/com/example/Ejercicio4.java
+++ src/main/java/com/example/Ejercicio4.java
@@ -3,6 +3,17 @@ package com.example;
 public class Ejercicio4 {
     public static void ejercicio4() {
         System.out.println("Ejercicio 4");
-        // tu código aquí
-    }
-}
+        // tu final double PI = 3.14159;
+        final int DIA_SEMANA = 7;
+        String NOMBRE_UNIVERSIDAD ="CESDE";
+
+        int radio = 5;
+        double area = PI * radio * radio;
+
+        System.out.println("El área del círculo es: " + area);
+        System.out.println("El valor de PI es: " + PI);
+        System.out.println("El día de la semana es: " + DIA_SEMANA);
+        System.out.println("El nombre de la universidad es: " + NOMBRE_UNIVERSIDAD);
+
+    {
+ 
