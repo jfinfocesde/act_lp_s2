65413c066da2c2f4ee5685dc175546577496e9ba
joseleonardosg19-ship-it <joseleonardosg19@gmail.com>
2025-09-13 14:08:44 -0500

    Update Ejercicio9.java

--- src/main/java/com/example/Ejercicio9.java
+++ src/main/java/com/example/Ejercicio9.java
@@ -4,5 +4,20 @@ public class Ejercicio9 {
     public static void ejercicio9() {
         System.out.println("Ejercicio 9");
         // tu código aquí
-    }
+    import java.util.Scanner;
+
+public class lowerCamelCase {
+
+  public static void main(String[] args) {
+   
+    int numeroDeEstudianteDeLaClase = 25;
+    double promedioDeCalifiacionesDelSemestre = 8.7;
+    String nombreCompletoDelProfesor = "Dr García";
+    boolean estudianteAprobado = true;
+  
+    System.out.println("El numero de estudiantes en la clase es:" + numeroDeEstudianteDeLaClase);
+    System.out.println("El promedio de calificaciones del semestre es:" + promedioDeCalifiacionesDelSemestre);
+    System.out.println("El nombre completo del profesor es:" + nombreCompletoDelProfesor);
+    System.out.println("¿Los estudiantes están aprobado?:" + estudianteAprobado);
+  }
 }
