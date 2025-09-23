ca18ee97fdcd30955855e0067210a7a25b1e1421
KingAskhar <dramirez12345@cesde.net>
2025-09-23 11:43:27 -0500

    actualizacion

--- src/main/java/com/example/Ejercicio6.java
+++ src/main/java/com/example/Ejercicio6.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
 public class Ejercicio6 {
     public static void ejercicio6() {
         System.out.println("Ejercicio 6");
-<<<<<<< HEAD
         // tu código aqui
-=======
-        // tu código aquí
->>>>>>> aa69d942fad4d8bbc9ad63ce8abf2e869ddce3e9
         System.out.println("Ingrese su nombre ");
         Scanner scanner = new Scanner(System.in);
         String nombre = scanner.nextLine();
