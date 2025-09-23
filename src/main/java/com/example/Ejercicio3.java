b2b847a61fcb921f0f65caa501f4e60bf6296b1b
KingAskhar <dramirez12345@cesde.net>
2025-09-23 10:58:00 -0500

    Update Ejercicio3.java

--- src/main/java/com/example/Ejercicio3.java
+++ src/main/java/com/example/Ejercicio3.java
@@ -4,5 +4,17 @@ public class Ejercicio3 {
     public static void ejercicio3() {
         System.out.println("Ejercicio 3");
         // tu código aquí
+        int numeroDeEstudiantes = 30;
+        System.out.println("El numero de estudiantes es " + numeroDeEstudiantes);
+
+        float promedioCalificaciones = 8.5f;
+        System.out.println("La calificacion es " + promedioCalificaciones);
+
+        String nombreCompleto = "David Ramirez Muriel";
+        System.out.println("Tu nombre es " + nombreCompleto);
+
+        boolean esMayorDeEdad = true;
+        System.out.println("El estudiante es mayor de edad? " + esMayorDeEdad);
+        // El nombre 'numeroDeEstudiantes' es válido porque usa camelCase, no contiene espacios y no contiene numeros al inicio
     }
 }
