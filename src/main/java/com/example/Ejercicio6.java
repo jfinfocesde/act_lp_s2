9a48d4acc21a980e11fea6cf405acae6a9e30a55
KingAskhar <dramirez12345@cesde.net>
2025-09-23 10:53:25 -0500

    Actualizacion ejercicios

--- src/main/java/com/example/Ejercicio6.java
+++ src/main/java/com/example/Ejercicio6.java
@@ -1,8 +1,20 @@
 package com.example;
-
+import java.util.Scanner;
 public class Ejercicio6 {
     public static void ejercicio6() {
         System.out.println("Ejercicio 6");
         // tu código aquí
+        System.out.println("Ingrese su nombre ");
+        Scanner scanner = new Scanner(System.in);
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingrese su edad");
+        int edad = scanner.nextInt();
+
+        System.out.println("Ingrese su estatura");
+        double estatura = scanner.nextDouble();
+        System.out.println("tu nombre es " + nombre + ", tienes " + edad + " años y mides " + estatura + " metros");
+        scanner.close();
+
     }
 }
