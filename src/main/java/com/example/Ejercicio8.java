e91acdd0b08f5df85942eb9a1124a1c3a075472f
talzateg-netizen <talzateg@gmail.com>
2025-09-24 07:08:00 -0500

    Update Ejercicio8.java

--- src/main/java/com/example/Ejercicio8.java
+++ src/main/java/com/example/Ejercicio8.java
@@ -3,6 +3,33 @@ package com.example;
 public class Ejercicio8 {
     public static void ejercicio8() {
         System.out.println("Ejercicio 8");
-        // tu código aquí
+package com.example;
+
+public class EjemploVariables {
+    public static void main(String[] args) {
+
+String miVariable = "hola"; /* Los nombres de variables deben comenzar con una letra minúscula. Si el nombre está compuesto por varias palabras, la primera letra de cada palabra debe ser mayúscula,(camelCase) */
+System.out.println("miVariable = " + miVariable);
+
+int _contador  = 10; /* permite comenzar con guion bajo  */
+System.out.println("_contador = " + _contador);
+
+double  $precio= 20.00; /* $ se usa para código generado automáticamente */
+System.out.println("$precio = " + $precio);
+
+int  variable123 = 12345; /* pueden contener números, siempre que no empiecen con un número. */
+ System.out.println("variable123 = " + variable123);
+
+
+/* /* /* 123variable  /* no puede empezar con número */
+/* mi-variable /* no se permiten espacios */ 
+/* class /* es una palabra clave usada para declarar clases */ 
+/* public /* es un modificador de acceso (public int edad;). */  
+  
+
+          }
+}
+
+        
     }
 }
