fd00f2021f39c338d97469459b753c1dd76a4769
Yorladis Motato <yorladismotatogrisales@gmail.com>
2025-09-25 12:38:11 -0500

    Agregué ejercicio HolaMundo

--- src/main/java/com/example/Ejercicio7.java
+++ src/main/java/com/example/Ejercicio7.java
@@ -3,6 +3,14 @@ package com.example;
 public class Ejercicio7 {
     public static void ejercicio7() {
         System.out.println("Ejercicio 7");
-        // tu código aquí
+        String nombre: "Juan";
+        String apellido: "Perez";
+    
+        String nombreCompleto : nombre + "" + apellido;
+        String saludo : "Hola". concat(nombreCompleto);
+    
+    System.out.println("nombre completo: " + nombreCompleto);
+
+     System.out.println(saludo);
     }
 }
