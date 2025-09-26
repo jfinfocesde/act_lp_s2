4b297efcedc2280ca4c5fc2c1995ce7a5615827f
Jhomilopez <jsanchez@interplay.com.co>
2025-09-26 11:23:52 -0500

    Update Ejercicio9.java

--- src/main/java/com/example/Ejercicio9.java
+++ src/main/java/com/example/Ejercicio9.java
@@ -4,5 +4,17 @@ public class Ejercicio9 {
     public static void ejercicio9() {
         System.out.println("Ejercicio 9");
         // tu código aquí
+
+        int numeroDeEstudiantesEnLaClase = 25;
+        System.out.println("El número de estudiantes en la clase es: " + numeroDeEstudiantesEnLaClase);
+
+        double promerioDeCalificacionesDelSemestre = 8.7;
+        System.out.println("El promedio de calificaciones del semestre es: " + promerioDeCalificacionesDelSemestre);
+
+        String nombreCompletoDelProfesor = "Dr. Garcia";
+        System.out.println("El nombre completo del profesor es: " + nombreCompletoDelProfesor);
+
+        boolean estudianteEstaAprobado = true;
+        System.out.println("¿El estudiante está aprobado? " + estudianteEstaAprobado);
     }
 }
