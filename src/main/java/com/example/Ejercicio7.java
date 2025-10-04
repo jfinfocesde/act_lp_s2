35fdb0a414753d849b38e51d38f6706a8b5526e0
talzateg-netizen <talzateg@gmail.com>
2025-10-04 10:42:48 -0500

    Update Ejercicio7.java

--- src/main/java/com/example/Ejercicio7.java
+++ src/main/java/com/example/Ejercicio7.java
@@ -3,11 +3,7 @@ package com.example;
 public class Ejercicio7 {
     public static void ejercicio7() {
         System.out.println("Ejercicio 7");
-       package com.example;
-
-public class CapturarDatos {
-    public static void main(String[] args) {
-
+       
 String nombre = "Juan";
 String apellido = "Perez";
 
@@ -20,9 +16,6 @@ int edad = 20;
 
 
 System.out.println(" tengo " + edad + " años" );
-
-          }
-}
 
     }
 }
